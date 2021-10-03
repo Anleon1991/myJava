1984838803388c1873395433d35d0ffb8bacc498
Anleon1991 <89403654+Anleon1991@users.noreply.github.com>
2021-10-03 09:04:36 +0300

    Add files via upload

---
+++ MyNewProject.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+public class MyNewProject {
+    public static void main(String[] args) {
+       int a = 2;
+       int b = 3;
+       int c = 6;
+       int z = ((a - 3) * b/2) + c;
+       System.out.println(z);
+    }
+}
