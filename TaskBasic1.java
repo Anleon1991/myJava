91d42daa418b4afd0c7159ad967341c4c9001a37
Anleon1991 <89403654+Anleon1991@users.noreply.github.com>
2022-01-20 15:51:55 +0300

    Task

---
+++ TaskBasic1.java
@@ -0,0 +1,9 @@
+public class TaskBasic1 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = 6;
+        int z = ((a - 3) * b/2) + c;
+        System.out.println(z);
+    }
+}
